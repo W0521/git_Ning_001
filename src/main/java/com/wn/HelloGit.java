b29435c2380116764bc53e45e3a6cc4ec4054dfe
Wangning <w05210720@163.com>
2021-03-14 11:06:58 +0800

    modify HelloGit

--- src/main/java/com/wn/HelloGit.java
+++ src/main/java/com/wn/HelloGit.java
@@ -3,7 +3,7 @@ package com.wn;
 public class HelloGit {
     public static void main(String[] args) {
         System.out.println("hello Git222");
-        System.out.println("hello");
+        System.out.println("hello222");
         System.out.println("haha in dev");
     }
 }
